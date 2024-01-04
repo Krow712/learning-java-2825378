26c90440332266397f1c9654390f1d9c6498d7cc
Krow712 <100172435+Krow712@users.noreply.github.com>
2024-01-04 09:32:30 -0600

    Update Main.java
    New java update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int student_Age = 15;
+        double student_GPA = 3.45;
+        char student_FirstInitial = 'K';
+        char student_LastInitial = 'H';
+        boolean hasPerfect_Attendance = true;
+
+        System.out.println(student_Age);
+        System.out.println(student_GPA);
+        System.out.println(student_FirsrInitial);
+        System.out.println(student_LastInitial);
+        System.out.println(hasPerfect_Attendance);
     }
 }
